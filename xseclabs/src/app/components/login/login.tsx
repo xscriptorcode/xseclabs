d0a8264eda75bdd933965e6c9326c18344d23bbf
xscriptorcode <preciado.oscar.osorio@gmail.com>
2025-09-28 17:52:08 +0200

    add login css

--- xseclabs/src/app/components/login/login.tsx
+++ xseclabs/src/app/components/login/login.tsx
@@ -3,17 +3,29 @@ import { useState } from "react"
 
 export default function Login () {
     const [user, setUser] = useState<string>("");
-    const [pass, serPass] = useState<string>("");
+    const [pass, setPass] = useState<string>("");
     
     return (
-        <div>
+        <div className="flex flex-col gap-2 justify-center text-center items-center">
+            
             <input 
             type="text"
             placeholder="Username"
             value={user}
             onChange={(e) => setUser(e.target.value)}
             className="border p-2 rounded"
-            />        
+            />
+            <input 
+            type="password"
+            placeholder="Password"
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+            className="border p-2 rounded"
+            />
+            <button className="text-white px-2 py-2 rounded hover:bg-red-500 border p-4 rounded">
+                Login
+            </button>     
         </div>
+
     )
 }
