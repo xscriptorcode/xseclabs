dcbede71360e216705e4079cdf97c1af54b62ca9
xscriptorcode <preciado.oscar.osorio@gmail.com>
2025-09-28 17:59:17 +0200

    upgrade login component

--- xseclabs/src/app/components/login/login.tsx
+++ xseclabs/src/app/components/login/login.tsx
@@ -6,26 +6,26 @@ export default function Login () {
     const [pass, setPass] = useState<string>("");
     
     return (
-        <div className="flex flex-col gap-2 justify-center text-center items-center border p-8 rounded">
-            
-            <input 
-            type="text"
-            placeholder="Username"
-            value={user}
-            onChange={(e) => setUser(e.target.value)}
-            className="border p-2 rounded"
-            />
-            <input 
-            type="password"
-            placeholder="Password"
-            value={pass}
-            onChange={(e) => setPass(e.target.value)}
-            className="border p-2 rounded"
-            />
-            <button className="text-white px-2 py-2 rounded hover:bg-red-500 border p-4 rounded">
-                Login
-            </button>     
+        <div className="flex flex-col items-center">
+            <div className="flex flex-col gap-2 justify-center text-center items-center border p-8 rounded-xl w-64">    
+                <input 
+                type="text"
+                placeholder="Username"
+                value={user}
+                onChange={(e) => setUser(e.target.value)}
+                className="border p-2 rounded-xl"
+                />
+                <input 
+                type="password"
+                placeholder="Password"
+                value={pass}
+                onChange={(e) => setPass(e.target.value)}
+                className="border p-2 rounded-xl"
+                />
+                <button className="text-white px-2 py-2 rounded hover:bg-red-500 border p-4 rounded">
+                    Login
+                </button>     
+            </div>
         </div>
-
     )
 }
