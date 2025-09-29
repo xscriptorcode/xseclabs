9a0228a92aa864f6faa2e0163e79b2508a032bdc
xscriptorcode <preciado.oscar.osorio@gmail.com>
2025-09-29 11:57:06 +0200

    define font

--- xseclabs/src/app/layout.tsx
+++ xseclabs/src/app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/footer/footer";
 import NavLinks, { Navbar } from "./components/navbar/navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "X Sec Lab",
@@ -27,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       ><NavLinks />
         <main>
         {children}
