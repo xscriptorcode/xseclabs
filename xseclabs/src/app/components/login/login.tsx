748eaaddbcf2d2d4ed43dc8cd520e0df6cc6fb19
xscriptorcode <preciado.oscar.osorio@gmail.com>
2025-09-30 17:21:39 +0200

    update login logic for supabase

--- xseclabs/src/app/components/login/login.tsx
+++ xseclabs/src/app/components/login/login.tsx
@@ -8,7 +8,54 @@ export default function Login () {
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState("");
-        
+    const router = useRouter();
+
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setLoading(true);
+        setError("");
+
+        try {
+            let loadingData;
+
+            //check if input is email or username
+
+            const isEmail = emailOrUsername.includes ('@');
+            if (isEmail){
+                loginData = await supabase.auth.signInWithPassword({
+                    email: emailOrUsername,
+                    password: password,
+                });
+            } else{
+                // login with username
+                const { data: profileData, error: profileError} = await supabase
+                .from('user_profiles')
+                .select('id_uuid, full_name')
+                .eq ('username', emailOrUsername)
+                .single();
+
+                if (profileError || !profileData){
+                    setError('User not found, please verify or register...');
+                    setLoading(false);
+                    return;
+                }
+
+                const { data: userData, error: userError} = await supabase
+                .from('users')
+                .select('email')
+                .eq('id_uuid', profileData.id_uuid)
+                .single();
+
+                if (userError || !userData) {
+                    setError('Error finding data');
+                    setLoading(false);
+                    return;
+                }
+
+            }
+        }
+    }
+
     return (
         <div className="flex flex-col items-center m-4">
             <div className="flex flex-col gap-2 justify-center text-center items-center border border-(--color-primary) p-8 rounded-xl w-64">    
