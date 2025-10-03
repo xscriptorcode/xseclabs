35d40318fa81139b7bad8f2be89b2c9604be58eb
xscriptorcode <preciado.oscar.osorio@gmail.com>
2025-10-03 20:34:05 +0200

    update footer styles

--- xseclabs/src/app/components/footer/footer.tsx
+++ xseclabs/src/app/components/footer/footer.tsx
@@ -1,6 +1,11 @@
 export default function Footer () {
     return(
-        <div className="flex justify-center text-center items-center p-2">
+        <div className="flex justify-center text-center items-center p-2"
+        style={{
+            backgroundColor: 'var (--color-surface)',
+            borderTop: '1px solid var (--color border)',
+            color: 'var(--color-text-secondary)'
+        }}>
             <p>Xseclabs / X©</p>
         </div>
     )
